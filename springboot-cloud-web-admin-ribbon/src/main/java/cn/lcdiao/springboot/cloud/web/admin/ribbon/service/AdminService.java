60cf4b2906414b2af92af2b71a249b456a77cbde
lcdiao <120879566@qq.com>
2019-08-01 10:39:29 +0800

    实现熔断机制（Ribbon）

--- springboot-cloud-web-admin-ribbon/src/main/java/cn/lcdiao/springboot/cloud/web/admin/ribbon/service/AdminService.java
+++ springboot-cloud-web-admin-ribbon/src/main/java/cn/lcdiao/springboot/cloud/web/admin/ribbon/service/AdminService.java
@@ -1,5 +1,6 @@
 package cn.lcdiao.springboot.cloud.web.admin.ribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -14,8 +15,14 @@ public class AdminService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "hiError")
     public String sayHi(String message) {
         return restTemplate.getForObject("http://springboot-cloud-service-admin/hi?message=" + message,String.class);
+    }
+
+    //熔断机制触发时（服务提供者宕机）执行该方法
+    public String hiError(String message) {
+        return "Hi，your message is :\"" + message + "\" but request error.";
     }
 
 }
